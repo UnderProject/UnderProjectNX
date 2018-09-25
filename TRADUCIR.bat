add5960e68f7787955bb7264860f49936ac048c5
jonyluke <jony8mb@gmail.com>
2018-09-25 17:32:31 +0200

    Update TRADUCIR.bat

--- TRADUCIR.bat
+++ TRADUCIR.bat
@@ -10,7 +10,7 @@ tools\hactool.exe -k keys.dat "%CD%\tools\extracted\eddd7cfa3cdca6623f31cf2f20e9
 if not exist "%CD%\READY_LAYEREDFS" (
 mkdir "%CD%\READY_LAYEREDFS\010080b00ad66000\romfs"
 )
-COPY %CD%\tools\extracted\romfs\game.win %CD%\READY_LAYEREDFS\010080b00ad66000\romfs
+COPY %CD%\tools\extracted\romfs\game.win %CD%\
 echo Applying Patch %progress%
-xdelta3.exe -d -vfs "game.win" "game Patch file.vcdiff" ".\READY_LAYEREDFS\game.win"
+xdelta3.exe -d -vfs "game.win" "game Patch file.vcdiff" ".\READY_LAYEREDFS\010080b00ad66000\romfs"
 pause
