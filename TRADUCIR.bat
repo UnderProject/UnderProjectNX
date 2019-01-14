971fc7ec064254a24e3dcbcc155815c58ac1565f
Raúl Álvarez Quereda <raulwii2@gmail.com>
2019-01-14 22:50:42 +0100

    Update TRADUCIR.bat

--- TRADUCIR.bat
+++ TRADUCIR.bat
@@ -6,7 +6,7 @@ cd tools
 titlekey.exe
 cd ..
 set /p var=<tools\final.txt
-tools\hactool.exe -k keys.txt "%CD%\tools\extracted\eddd7cfa3cdca6623f31cf2f20e9257e.nca" --titlekey="%var%" --romfsdir="%CD%\tools\extracted\romfs"
+tools\hactool.exe -k keys.dat "%CD%\tools\extracted\eddd7cfa3cdca6623f31cf2f20e9257e.nca" --titlekey="%var%" --romfsdir="%CD%\tools\extracted\romfs"
 if not exist "%CD%\READY_LAYEREDFS" (
 mkdir "%CD%\READY_LAYEREDFS\010080b00ad66000\romfs"
 )
